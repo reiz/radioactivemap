592f839a654434ebe17fb5f3e29263ba41bd1ef1
Robert Reiz <robert.reiz@gmx.com>
2011-04-01 23:24:01 -0700

    clean up javascript

--- public/javascripts/gmap.js
+++ public/javascripts/gmap.js
@@ -4,16 +4,15 @@ var prev_marker
 
 function initialize() {
     var map_for_landing_page = document.getElementById("map_for_landing_page");
-    if (map_for_landing_page != null){
-        initialize_map_for_landing_page();
-    }
     var map_for_measurement  = document.getElementById("map_for_measurement");
-    if (map_for_measurement != null){
-        initialize_map_for_measurement();
+    if (map_for_landing_page != null){
+        initialize_map_for_landing_page(map_for_landing_page);
+    } else if (map_for_measurement != null){
+        initialize_map_for_measurement(map_for_measurement);
     }
 }
 
-function initialize_map_for_landing_page() {
+function initialize_map_for_landing_page(htmlElement) {
     var latlng = new google.maps.LatLng(37.75, 140.4666667);
     var latlng2 = new google.maps.LatLng(38.75, 140.4666667);
     var myOptions = {
@@ -21,7 +20,7 @@ function initialize_map_for_landing_page() {
         center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    map = new google.maps.Map(document.getElementById("map_for_landing_page"), myOptions);
+    map = new google.maps.Map(htmlElement, myOptions);
 
     var contentString = '<div id="content">'+
             '<div id="siteNotice">'+
@@ -55,14 +54,14 @@ function initialize_map_for_landing_page() {
     });
 }
 
-function initialize_map_for_measurement() {
+function initialize_map_for_measurement(htmlElement) {
     var latlng = new google.maps.LatLng(37.75, 140.4666667);
     var myOptions = {
         zoom: 5,
         center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    map = new google.maps.Map(document.getElementById("map_for_landing_page"), myOptions);
+    map = new google.maps.Map(htmlElement, myOptions);
     google.maps.event.addListener(map, 'click', function(event) {
         placeManualMarker(event.latLng);
     });
